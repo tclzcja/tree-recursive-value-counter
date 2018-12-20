4a1be1d43dae98f4b75404df68a28ab4b4eb4841
JingAn Chen <lzsoft.cja@gmail.com>
2018-12-20 12:21:31 -0500

    Done.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BloombergCodingExercise
+{
+    class Node
+    {
+        public string value { get; private set; }
+        public List<Node> children { get; set; }
+        public Node(string v)
+        {
+            this.value = v;
+            this.children = new List<Node>();
+        }
+        public Dictionary<string, int> printoutValueCount()
+        {
+            Dictionary<string, int> valueCountMap = new Dictionary<string, int>();
+            this.recursiveChildren(valueCountMap);
+            // foreach (KeyValuePair<string, int> c in valueCountMap)
+            // {
+            //     Console.WriteLine(c.Key + ' ' + c.Value);
+            // }
+            return valueCountMap;
+        }
+        private void recursiveChildren(Dictionary<string, int> map)
+        {
+            if (map.ContainsKey(this.value))
+            {
+                map[this.value]++;
+            }
+            else
+            {
+                map.Add(this.value, 1);
+            }
+            foreach (Node child in this.children)
+            {
+                child.recursiveChildren(map);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = new BloombergCodingExercise.Node("apple");
+            var n2 = new BloombergCodingExercise.Node("orange");
+            n1.children.Add(n2);
+            var n3 = new BloombergCodingExercise.Node("orange");
+            n1.children.Add(n3);
+            n2.children.Add(new BloombergCodingExercise.Node("apple"));
+            n2.children.Add(new BloombergCodingExercise.Node("apple"));
+            n3.children.Add(new BloombergCodingExercise.Node("apple"));
+            n3.children.Add(new BloombergCodingExercise.Node("apple"));
+            n3.children.Add(new BloombergCodingExercise.Node("apple"));
+            n1.printoutValueCount();
+        }
+    }
+}
